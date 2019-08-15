f471a75ee17d9742f724556589eeeb5a57df001c
Pedram <tadayoni@gmail.com>
2019-08-14 22:40:29 -0700

    fix: added eks_worker ami

--- terraform/worker_node_asg.tf
+++ terraform/worker_node_asg.tf
@@ -1,3 +1,12 @@
+data "aws_ami" "eks-worker" {
+  filter {
+    name   = "name"
+    values = ["amazon-eks-node-${aws_eks_cluster.books.version}-v*"]
+  }
+
+  most_recent = true
+  owners      = ["602401143452"] # Amazon EKS AMI Account ID
+}
 
 data "aws_region" "current" {}
 
