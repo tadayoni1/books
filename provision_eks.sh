ecc67a61e55b534de93dfdfab57854d1d7d0fc4c
Pedram <tadayoni@gmail.com>
2019-08-14 22:50:35 -0700

    fix: terraform output now runs inside terraform folder

--- provision_eks.sh
+++ provision_eks.sh
@@ -13,17 +13,17 @@ if [ -z $TERRAFORM_MODE ]; then
   TERRAFORM_MODE="apply"
 fi
 
+mkdir ~/terraform-books/
+mkdir ~/terraform-books/output/
+
 pushd terraform
 terraform init -backend-config="key=state/books/eks_backend_config"
 
 terraform ${TERRAFORM_MODE} -var-file="variables.tfvars" -var-file="../../secret.tfvars"
-popd
-
-mkdir ~/terraform-books/
-mkdir ~/terraform-books/output/
 
 kubeconfig=$(terraform output "kubeconfig")
 config_map_aws_auth=$(terraform output "config_map_aws_auth")
+popd
 
 echo $kubeconfig > ~/terraform-books/output/kubeconfig
 echo $config_map_aws_auth > ~/terraform-books/output/config_map_aws_auth
