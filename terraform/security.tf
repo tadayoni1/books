ceec6c88c11534e813c9f97726536db84b885d32
Pedram <tadayoni@gmail.com>
2019-08-14 21:53:47 -0700

    feat: security resources for eks

---
+++ terraform/security.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role" "books-cluster" {
+  name = "terraform-eks-books-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "books-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.books-cluster.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "books-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.books-cluster.name}"
+}
