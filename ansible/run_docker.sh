eb72116aa868d1c0977dd10d7c990c1c6191d996
Ubuntu <ubuntu@ip-172-31-23-148.us-west-2.compute.internal>
2019-08-17 06:11:10 +0000

    feat: build docker with ansible

--- ansible/run_docker.sh
+++ ansible/run_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Build image and add a descriptive tag
-docker build --tag=tirganbooks .
+docker build -t tirganbooks -f ../python/ .
 
 # Run flask app
 docker run -p 8080:8080 -idt tirganbooks
